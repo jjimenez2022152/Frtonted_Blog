6f8fa4426e8925f32001ca7ebe0813c9893ed791
jjimenez2022152 <jjimenez-2022152@kinal.edu.gt>
2024-05-10 00:38:50 -0600

    Se trabajan imagenes, links y boton de ver mas y menos

--- src/pages/ProjectsList.jsx
+++ src/pages/ProjectsList.jsx
@@ -1,9 +1,8 @@
-import { useEffect, useState } from "react"
+import { useState } from "react";
 import { usePost } from "../hooks/usePosts";
-import '../App.css'
+import '../App.css';
 
-
-const URL = "http://localhost:8080/gestorOpiniones/v1/publications"
+const URL = "http://localhost:8080/gestorOpiniones/v1/publications";
 
 export const ProjectsList = () => {
     const { posts, getPost, isFetching } = usePost();
@@ -13,25 +12,47 @@ export const ProjectsList = () => {
     }
 
     if (isFetching) {
-        return <div className="">cargando</div>
+        return <div className="">cargando</div>;
     }
 
-    console.log(posts)
+    console.log(posts);
 
-
-    const postsUI = posts.map((post) => <CardPost titulo={post.titulo} key={post.titulo} texto={post.texto} avatarUrl={post.avatarUrl} />);
+    const postsUI = posts.map((post) => (
+        <CardPost
+            titulo={post.titulo}
+            key={post.titulo}
+            texto={post.texto}
+            avatarUrl={post.avatarUrl}
+            gitHub={post.gitHub}
+        />
+    ));
 
     return postsUI;
-}
+};
 
-function CardPost({ titulo, texto, avatarUrl }) {
+function CardPost({ titulo, texto, avatarUrl, gitHub }) {
+    const [showMore, setShowMore] = useState(false);
+
+    const toggleShowMore = () => {
+        setShowMore(!showMore);
+    };
+
+    const toggleShowLess = () => {
+        setShowMore(false);
+    };
+
     return (
-        <><div className="publications-all">
+        <div className="publications-all">
             <div>{titulo}</div>
-            <div>{texto}</div>
-            <div>{avatarUrl}</div>
+            {!showMore && <button id="ver-mas" className="button" onClick={toggleShowMore}>Ver más</button>}
+            {showMore && (
+                <>
+                    <div>{texto}</div>
+                    <div><img src={avatarUrl} alt="Avatar" className="avatar-image" /></div>
+                    <div><a href={gitHub} target="_blank" rel="noopener noreferrer">Ver en GitHub</a></div>
+                    <button id="ver-menos" className="button button-outline" onClick={toggleShowLess}>Ver menos</button>
+                </>
+            )}
         </div>
-        </>
-
-    )
+    );
 }
